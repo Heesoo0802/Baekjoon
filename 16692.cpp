43a4986dcf3db7bb795c98b99839e9182b2cba11
Heesoo0802 <157887544+Heesoo0802@users.noreply.github.com>
2025-04-14 11:34:38 +0900

    16692 github

---
+++ 16692.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+struct counter
+{
+    int cn; //counter number
+    int ts; //time sum
+};
+struct comp
+{
+    bool operator()(counter a, counter b)
+    {
+        if(a.ts == b.ts) return a.ts > b.ts;
+        return a.ts > b.ts; //priority queue front = min
+    }
+};
+int main()
+{
+    int n, c;
+    cin >> n >> c;
+    priority_queue <counter, vector<counter>, comp> pq;
+    for(int i=0;i<c;i++)
+    {
+        int time;
+        cin >> time;
+
+        if(i<n) // i = 0 ~ n-1
+        {
+            pq.push({i+1, time});
+            cout << i+1 << " ";
+        }
+        else
+        {
+            counter top = pq.top();
+            pq.pop();
+            pq.push({top.cn, top.ts + time});
+            cout << top.cn << " ";
+        }
+    }
+}
